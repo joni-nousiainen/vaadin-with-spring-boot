07790bebd5764fa3e215ecfc034701f89bf17f7d
Joni Nousiainen <joni.nousiainen@codecenter.fi>
2015-03-12 20:43:13 +0200

    Initial import.

---
+++ README.md
@@ -0,0 +1,1 @@
+Run the application by typing `mvn spring-boot:run`, or package it with `mvn clean package` and then run it with `java -jar target/<JAR filename>`.
---
+++ src/main/java/info/joninousiainen/examples/vaadin/AppUI.java
@@ -0,0 +1,29 @@
+package info.joninousiainen.examples.vaadin;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import org.vaadin.spring.VaadinUI;
+
+@VaadinUI
+@Widgetset("AppWidgetset")
+@Theme("valo")
+public class AppUI extends UI {
+    @Override
+    protected void init(VaadinRequest request) {
+        Page.getCurrent().setTitle("App");
+
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
+        layout.setMargin(true);
+        layout.setSpacing(true);
+
+        layout.addComponent(new Label("Hello Vaadin!"));
+
+        this.setContent(layout);
+    }
+}
